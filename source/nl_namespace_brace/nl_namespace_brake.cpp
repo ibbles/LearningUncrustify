224d4b28191817ab9fe0b4f3d28c2e60de86445b
Martin Nilsson <ibbles@hotmail.com>
2025-05-25 18:31:08 +0200

    Minimize changes made to nl_namespace_brake.cpp

--- source/nl_namespace_brace/nl_namespace_brake.cpp
+++ source/nl_namespace_brace/nl_namespace_brake.cpp
@@ -1,7 +1,6 @@
 
 /*
     Multiple namespaces on the same line.
-    Namespace contents not indented.
  */
 
 namespace A { namespace B { namespace C { } } }
@@ -17,47 +16,53 @@ MyClass() {
 
 /*
     Each namespace on its own line, brace on the same line.
-    Namespace content indented.
  */
 
 namespace A {
-	namespace B {
-		namespace C {
-		}
-	}
+namespace B {
+namespace C {
+}
+}
 }
 
 namespace A {
-	namespace B {
-		namespace C {
-			class MyClass { MyClass() { doInitialization(); }};
-		}
-	}
+namespace B {
+namespace C {
+class MyClass {
+MyClass() {
+	doInitialization();
+}
+};
+}
+}
 }
 
 
 /*
     Each namespace on its own line, brace on a new line.
-    Namespace content indented.
  */
 
 namespace A
 {
-	namespace B
-	{
-		namespace C
-		{
-		}
-	}
+namespace B
+{
+namespace C
+{
+}
+}
 }
 
 namespace A
 {
-	namespace B
-	{
-		namespace C
-		{
-			class MyClass { MyClass() { doInitialization(); }};
-		}
-	}
+namespace B
+{
+namespace C
+{
+class MyClass {
+MyClass() {
+	doInitialization();
+}
+};
+}
+}
 }
